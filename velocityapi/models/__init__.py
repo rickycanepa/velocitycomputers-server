30c47c34dc57e5fd05a7e3756caac5ecfb8aeefe
Ricky Canepa <rickycanepa.rc@gmail.com>
2023-03-14 10:23:51 -0500

    computer model created

---
+++ velocityapi/models/__init__.py
@@ -0,0 +1,2 @@
+from .computer import Computer
+from .power_supply import PowerSupply
---
+++ velocityapi/models/computer.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Computer(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name='computer') 
+    power_supply = models.ForeignKey("PowerSupply", on_delete=models.CASCADE, related_name='computer')
+    processor = models.ForeignKey("Processor", on_delete=models.CASCADE, related_name='computer')
+    gpu = models.ForeignKey("GPU", on_delete=models.CASCADE, related_name='computer')
+    motherboard = models.ForeignKey("Motherboard", on_delete=models.CASCADE, related_name='computer')
+    ram = models.ForeignKey("RAM", on_delete=models.CASCADE, related_name='computer')
+    case = models.ForeignKey("Case", on_delete=models.CASCADE, related_name='computer')
+    cpu_cooler = models.ForeignKey("CpuCooler", on_delete=models.CASCADE, related_name='computer')
+    keyboard = models.ForeignKey("Keyboard", on_delete=models.CASCADE, related_name='computer')
+    mouse = models.ForeignKey("Mouse", on_delete=models.CASCADE, related_name='computer')
+    ssd = models.ForeignKey("SSD", on_delete=models.CASCADE, related_name='computer')
+    price = models.PositiveIntegerField()
+    likes = models.IntegerField(default=0)  
