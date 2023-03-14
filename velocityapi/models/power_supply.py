111d16bf48668e3b4702afd73586c4e9e46b8b1b
Ricky Canepa <rickycanepa.rc@gmail.com>
2023-03-14 10:34:07 -0500

    created PowerSupply model

--- velocityapi/models/power_supply.py
+++ velocityapi/models/power_supply.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 class PowerSupply(models.Model):
-    title = models.CharField 
+    title = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    power = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    efficiency = models.CharField(max_length=100)
